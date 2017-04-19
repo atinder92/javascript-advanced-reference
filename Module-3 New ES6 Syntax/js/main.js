500011970ba2285e0a1e32032b8741275dc25060
Atinder Singh <atinder.s.92@gmail.com>
2017-04-19 15:02:51 +1000

    New ES6 syntax

---
+++ Module-3 New ES6 Syntax/js/main.js
@@ -0,0 +1,111 @@
+// Author   : Atinder Singh
+// Date     : 19/4/2017
+
+'use strict';
+
+//___________________________________________
+
+//USING LET AND CONST KEYWORD IN ES6 ////////
+//___________________________________________
+
+
+/*
+** Use let keyword to define variables
+** Use const keyword to define contants
+*/
+
+let name = "Atinder"; // variable defined at global scope
+const VERSION = 1.0;  // constant defined at global scope
+
+function displayName(){
+    
+    let myName = "Atinder Pal"; // this variable is only defined to this function scope
+    console.log(name);
+
+}
+
+//displayName();
+
+//console.log(myName); // cannot access myName
+
+
+
+
+//___________________________________________
+
+// USING ARROW FUNCTION IN ES6 //////////////
+//___________________________________________
+
+
+//showPrice not taking any parameter
+let showPrice = () => 4.5; 
+
+console.log(showPrice());
+
+//calculatePrice taking one parameter
+
+let calculatePrice = (money) => {
+
+    return money*money;
+};
+
+//console.log("Calculating price: "+calculatePrice(4));
+
+//One important point to remember about using => is that, in => functions 'this' refers to the context in which function is running
+//We cannot use bind, call and apply functions to override 'this' keyword
+
+
+let person = {
+    
+    age:24,
+    displayAge: () => console.log(this)
+
+
+};
+
+
+
+
+//person.displayAge(); // console's window object
+
+
+let college = {
+
+    
+    name:"Harvard",
+    displayInfo: function(){
+        person.displayAge();
+    }
+    
+
+};
+
+
+//college.displayInfo(); // console's window object again
+
+
+//___________________________________________
+
+// DEFAULT FUNCTIONS PARAMETERS /////////////
+//___________________________________________
+
+
+function displayProduct(productId = 100){
+
+    console.log("Displaying information for the product with ID: "+productId);
+
+}
+
+//If you pass a value, it will not look for default value's, 
+//But if you don't pass value's, it will look for default value's 
+displayProduct(300);
+
+
+
+//___________________________________________
+
+// REST AND SPREAD //////////////////////////
+//___________________________________________
+
+
+
