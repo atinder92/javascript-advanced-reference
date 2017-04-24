f350ba7edda18f8c8b4a19b564d7bf9b06f58682
Atinder Singh <atinder.s.92@gmail.com>
2017-04-24 23:10:06 +1000

    Created very simple class

---
+++ Module-4 Modules and Classes/classes/person.js
@@ -0,0 +1,19 @@
+'use strict;'
+
+class Person {
+
+    constructor(){
+    console.log("Instantiating Object");
+    
+    }
+    
+    sayHello(){
+        console.log("Hello how are you");
+    
+    }
+
+
+}
+
+var person = new Person();
+person.sayHello();    
