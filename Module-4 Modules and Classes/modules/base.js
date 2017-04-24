3e3886603e2dcad898d845d26a34e2169326c39b
Atinder Singh <atinder.s.92@gmail.com>
2017-04-24 22:47:52 +1000

    Using Modules in es6

--- Module-4 Modules and Classes/modules/base.js
+++ Module-4 Modules and Classes/modules/base.js
@@ -1,1 +1,17 @@
 console.log("--- Welcome to Base Module ---");
+
+//importing values from module1.js
+//import {carMaker,carModel} from './module1.js';
+
+//we can also import functions from other modules
+import {carMaker,carModel,displayInfo} from './module1.js';
+
+//we can also use
+//import {carMaker as carManufacturer,carModel} from './module1.js';
+
+
+//console.log("CarMaker is "+carMaker);
+//console.log("CarModel is "+carModel);
+displayInfo();
+
+
---
+++ Module-4 Modules and Classes/modules/module1.js
@@ -0,0 +1,13 @@
+let carMaker = 'Toyota';
+let carModel = 2014;
+
+function displayInfo(){
+    console.log("Displaying information about Car");
+    console.log(`${carMaker} ${carModel}`);
+
+}
+//exporting two variables
+export {carMaker,carModel,displayInfo};
+
+
+
