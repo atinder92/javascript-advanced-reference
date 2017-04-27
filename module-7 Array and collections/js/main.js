a1633878af78114ad88aa84dc5d2ff0064b71703
Atinder Singh <atinder.s.92@gmail.com>
2017-04-27 10:52:53 +1000

    Arrays and Maps in ES6

--- module-7 Array and collections/js/main.js
+++ module-7 Array and collections/js/main.js
@@ -1,122 +1,99 @@
 // Author   : Atinder Singh
-// Date     : 25/4/2017
+// Date     : 27/4/2017
 
 'use strict';
 
+//-------------------------------------------
+
+//USING ARRAYS IN ES6 ///////////////////////
 //___________________________________________
 
-//USING SYMBOLS IN ES6 //////////////////////
+console.log("----- Arrays in ES6 -----");
+
+//Array class in ES6 has useful methods described below:
+
+//Array.of()
+//creating array
+let ids = Array.of(101,102,103);
+console.log(ids);
+
+//Creating new array from existing array
+
+let newIds = Array.from(ids,v=> v+10);
+
+console.log(newIds);
+
+
+newIds.fill(200); // replaces all value of newIds array with 200
+
+console.log(newIds);
+
+//we can also use the following
+
+ids.fill(900,1); // start replacing values in ids array, starting from index 1
+
+console.log(ids);
+
+
+
+
+//for more methods, check reference
+
+
+
+
+
+
+
+//-------------------------------------------
+
+//USING MAPS IN ES6 /////////////////////////
 //___________________________________________
 
-//Symbol creates a unique identifier
+console.log("----- MAPS in ES6 -----");
 
-let mySymbol = Symbol('first symbol');
+let user1 = {
+    id:1,
+    name:'Atinder'
 
-console.log(mySymbol);
+};
 
-console.log(Symbol.for('first symbol'));
+let user2 = {
 
-//___________________________________________
-
-//STRING EXTENSIONS IN ES6 //////////////////
-//___________________________________________
-
-
-let myString = "Computer programming is awesome";
-
-//startsWith
-
-console.log(myString.startsWith("Computer"));
-
-//endsWith
-console.log(myString.endsWith("awesome"));
-
-//includes
-
-console.log(myString.includes("program"));
-
-//using emoji
-let emojiString = "Looks amazing \u{1f3c4}";
-
-console.log(emojiString);
-
-
-//repeat
-let greet = "Welcome ";
-
-let greetTenTimes = greet.repeat(10);
-
-console.log(greetTenTimes);
-
-
-
-//___________________________________________
-
-//NUMBER EXTENSIONS IN ES6 //////////////////
-//___________________________________________
-
-
-let numberA = 10;
-let numberB = 20;
-
-
-
-console.log(Number.parseInt(numberA));
-
-//we also have Number.parseFloat()
-//Number.isNaN() 
-//Number.isFinite()
-//Number.MAX_SAFE_INTEGER
-//Number.MIN_SAFE_INTEGER
-
-
-
-
-//___________________________________________
-
-//MATH EXTENSIONS IN ES6 ////////////////////
-//___________________________________________
-
-
-//Math object has a number of new functions, which we can use to perform mathematics operations
-//For more information, you can visit mozilla developer network
-
-console.log(Math.cbrt(64));
-
-
-console.log(Math.trunc(2.34));
-
-
-//___________________________________________
-
-//REGEXP EXTENSIONS IN ES6 //////////////////
-//___________________________________________
-
-
-
-//___________________________________________
-
-//FUNCTIONS EXTENSIONS IN ES6 ///////////////
-//___________________________________________
-
-//we have name property, which gives us the name of the function
-
-let displayInformation = function (){
-
-console.log("Displaying Information");
+    id:2,
+    name:'Mani'
 
 }
 
 
-console.log(displayInformation.name);
+//creating a map
+//here the objects are mapped to values such as cleaning, smiling
 
-let a = function sayHello(){
+let userTasks = new Map();
 
-console.log("Hello");
+userTasks.set(user1,'Cleaning');
+userTasks.set(user2,'Smiling');
 
-}
+console.log(userTasks);
+console.log(userTasks.get(user1));
+console.log(userTasks.size);
+console.log(userTasks.has(user1));
 
-console.log(a.name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
